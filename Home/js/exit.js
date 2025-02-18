d5afbcda9df3ba826820f69895f77f87f6d5a109
Talaat <talaatgamal.dev@gmail.com>
2025-02-18 18:12:23 +0200

    first commit

---
+++ Home/js/exit.js
@@ -0,0 +1,12 @@
+
+
+
+const workspace = document.getElementById("exit-window")
+
+document.querySelectorAll(".begin-now").forEach(function(element) {
+  element.addEventListener("click", function() {
+    workspace.style.left="0vw"
+  });
+});
+
+
